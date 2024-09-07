ccafea760c19294a0164111aa7a74c2652327da2
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-07 10:32:14 +0530

    ChildComponent

---
+++ src/ChildComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function ChildComponent({sendMessage}){
+    const handleMessage = ()=>{
+        const message = "Message from Child"
+        sendMessage(message)
+    }
+
+    return (
+        <div>
+            <button onClick={handleMessage}>Send message to Parent</button>
+        </div>
+    )
+}
+
+export default ChildComponent
