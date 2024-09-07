e8a3e4e23405acf3ff56f11ad257750c6c404f83
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-07 10:31:13 +0530

    CounterAdded

---
+++ src/Counter.jsx
@@ -0,0 +1,29 @@
+import React,{useState} from "react";
+
+function Counter(){
+    
+    const [count,setCount] = useState(0)
+
+    const increment = ()=>{
+        if(count<10){
+            setCount(count+1)
+        }
+    }
+    const decrement = ()=>{
+        if(count>0){
+            setCount(count-1)
+        }
+    }
+    
+
+    return (
+        <div>
+            <button onClick={increment}>Increment</button>
+            <h1>{count}</h1>
+            <button onClick={decrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default Counter
+
