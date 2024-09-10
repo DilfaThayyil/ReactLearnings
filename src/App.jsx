2bd2ce2ae34329ee7839f2a318e40f725c2b0566
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-10 15:33:53 +0530

    axiosAdded

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import React,{useState} from "react";
-import ChildComponent from "./ChildComponent";
+import React,{useEffect, useState} from "react";
+import axios from 'axios'
 
 function App(){
   const [count,setCount] = useState(0)
+  const [user,setUser] = useState(null)
+  const [loading,setLoading] = useState(false)
+  const [error,setError] = useState('')
+
+  useEffect(()=>{
+    if(count >= 1 && count <= 10){
+      fetchUser(count)
+    }else{
+      setUser(null)
+    }
+  },[count])
+
+  const fetchUser = async(count) =>{
+    const data = await axios.get(`https://jsonplaceholder.typicode.com/users/${count}`)
+  }
 
   function handleIncrement(){
     setCount(count+1)
