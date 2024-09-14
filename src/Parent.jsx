799ea0f53ecb24ca8ce394c7f20be2d3a1ea858c
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-14 12:44:13 +0530

    new parent component

---
+++ src/Parent.jsx
@@ -0,0 +1,14 @@
+import Child from "./Child";
+import { useState } from "react";
+
+function Parent(){
+    const [data,setData] = useState('')
+  return (
+    <div>
+      <p>Data from Child : {data}</p>
+      <Child setData={setData}/>
+    </div>
+  )
+}
+
+export default Parent
