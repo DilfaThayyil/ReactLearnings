aec3e4d68813a9883426e01e21e4d6535a8f047d
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-17 11:59:05 +0530

    counter app in ReducerFunction

--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -2,7 +2,15 @@ import { useState } from "react";
 
 
 function ReducerFunction(){
-    
+    const [count,setCount] = useState(0)
+
+    return (
+        <>
+        <h1>{count}</h1>
+        <button onClick={()=>setCount(prev => prev + 1)}>Increment</button>
+        <button onClick={()=>setCount(prev => prev - 1)}>Decrement</button>
+        </>
+    )
 }
 
 export default ReducerFunction
