480851c64a0774f8d2b8e9e550f3712fb56bb73e
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-17 14:47:46 +0530

    modified

--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -1,38 +1,31 @@
-import { useState } from "react";
-import { useReducer } from "react";
+import { useReducer } from "react"
 
+ function ReducerFunction (){
+    const initialState = {count:0}
 
-function ReducerFunction(){
-
-    const initialState =  {count:0}
-
-    const reducer = (state,action)=>{
+    const reducer = ((state,action)=>{
         switch(action.type){
-            case 'increase':{
-                return {count: state.count + 1}
+            case 'increment':{
+                return {count:state.count+1}
             }
-            case 'decrease':{
-                return {count: state.count - 1}
-            }
-            case 'input':{
-                return {count:action.payload}
+            case 'decrement':{
+                return {count:state.count-1}
             }
             default :{
                 return state
             }
         }
-    }
+    })
 
     const [state,dispatch] = useReducer(reducer,initialState)
 
     return (
         <>
         <h1>{state.count}</h1>
-        <button onClick={()=>dispatch({type:'increase'})}>Increment</button>
-        <button onClick={()=>dispatch({type:'decrease'})}>Decrement</button><br />
-        <input value={state.count} onChange={(e)=>dispatch({type:'input',payload:Number(e.target.value)})} type="number" />
+        <button onClick={()=>dispatch({type:'increment'})}>Increment</button>
+        <button onClick={()=>dispatch({type: 'decrement'})}>Decrement</button>
         </>
     )
-}
+ }
 
-export default ReducerFunction
+ export default ReducerFunction
