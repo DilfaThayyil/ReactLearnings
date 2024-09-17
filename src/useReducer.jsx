cc3f01e96301f632ef8ed586f03d27601672ea79
DilfaThayyil <dilfathayyil@gmail.com>
2024-09-17 12:38:33 +0530

    counter app using useReducer

--- src/useReducer.jsx
+++ src/useReducer.jsx
@@ -1,14 +1,32 @@
 import { useState } from "react";
+import { useReducer } from "react";
 
 
 function ReducerFunction(){
-    const [count,setCount] = useState(0)
+
+    const initialState =  {count:0}
+
+    const reducer = (state,action)=>{
+        switch(action.type){
+            case 'increase':{
+                return {count: state.count + 1}
+            }
+            case 'decrease':{
+                return {count: state.count - 1}
+            }
+            default :{
+                return state
+            }
+        }
+    }
+
+    const [state,dispatch] = useReducer(reducer,initialState)
 
     return (
         <>
-        <h1>{count}</h1>
-        <button onClick={()=>setCount(prev => prev + 1)}>Increment</button>
-        <button onClick={()=>setCount(prev => prev - 1)}>Decrement</button>
+        <h1>{state.count}</h1>
+        <button onClick={()=>dispatch({type:'increase'})}>Increment</button>
+        <button onClick={()=>dispatch({type:'decrease'})}>Decrement</button>
         </>
     )
 }
